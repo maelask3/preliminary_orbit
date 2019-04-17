29196c74da10df7cec33dd821052756928d7661c
misantam <millanpracticas98@gmail.com>
2019-04-17 16:26:43 +0200

    Revert "Añadida variante de roots()"
    This reverts commit 04ef92d3919c61589b1df6808e154c448695b4fc.

--- MatlabUtils.c
+++ MatlabUtils.c
@@ -4,7 +4,6 @@
 #include <complex.h>
 #include <stdbool.h>
 #include <string.h>
-#include "rpoly.h"
 
 double norm(double *v)
 {
@@ -47,28 +46,7 @@ double det2x2(double m[][2])
 
 double *roots(double *v)
 {
-	double *real, *imagin;
-	real = (double*) malloc(15*sizeof(double));
-	imagin = (double*) malloc(15*sizeof(double));
-	int info[15];
-	int solutions = rpoly( v, 15, real, imagin, info);
-	
-	double aux[solutions];
-	int j = 0;
-	for(int i = 0; i< solutions; i++){
-		if(fabs(imagin[i]) == 0.0){
-			aux[j] = real[i];
-			j++;
-		}
-	}
-
-	double *dev;
-	dev = (double*) malloc(j*sizeof(double));
-	for(int i = 0; i<j; i++){
-		dev[i] = aux[i];
-	}
-
-	return dev;	
+	return 1;
 }
 
 bool isreal(double complex n)
